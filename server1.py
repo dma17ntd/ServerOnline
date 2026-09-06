0603729afbc0cd584f1d7a67402e52db0e352a51
manhscuti <158720810+dma17ntd@users.noreply.github.com>
2026-09-06 15:59:57 +0700

    Update server1.py

--- server1.py
+++ server1.py
@@ -410,9 +410,9 @@ class LinuxLogoGlitchV3:
 
 if __name__ == "__main__":
     LinuxLogoGlitchV3(
-        fps=60,
+        fps=70,
         glitch_duration=1.5,
-        palette_speed=3.5,
+        palette_speed=9.5,
         palette_min_interval=0.9,
         palette_max_interval=1.5,
     ).run()
