23209378114f37dfc2771eca53cd7008fbdf1761
manhscuti <158720810+dma17ntd@users.noreply.github.com>
2026-09-06 15:57:53 +0700

    DMAS: update server1.py

--- server1.py
+++ server1.py
@@ -413,6 +413,6 @@ if __name__ == "__main__":
         fps=60,
         glitch_duration=1.5,
         palette_speed=5.5,
-        palette_min_interval=2.0,
-        palette_max_interval=4.5,
+        palette_min_interval=0.9,
+        palette_max_interval=1.5,
     ).run()
